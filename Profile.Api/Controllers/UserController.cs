028792b09164b843550ad3ffae62f12bf0218ddd
Oleh Khavar <olehkhavar@gmail.com>
2025-05-11 16:39:18 +0300

    add update information

--- Profile.Api/Controllers/UserController.cs
+++ Profile.Api/Controllers/UserController.cs
@@ -1,6 +1,7 @@
 using FluentResults;
 using Microsoft.AspNetCore.Mvc;
 using Profile.Api.Attributes;
+using Profile.Application.DTOs;
 using Profile.Application.Interfaces;
 using Shared.DTOs;
 using Shared.Interfaces;
@@ -27,5 +28,36 @@ public class UserController(ITokenParser tokenParser, IUserService userService) 
         }
 
         return Ok(new ResultSuccessDto<List<string>>(result.IsSuccess, result.Value));
+    }
+
+    [HttpPut("information")]
+    public async Task<IActionResult> Information([FromBody] UserUpdateDto userUpdate)
+    {
+        string authHeader = Request.Headers.Authorization.FirstOrDefault() ?? string.Empty;
+        TokenInformation tokenInformation = tokenParser.ParseToken(authHeader);
+
+        userUpdate.OldUserEmail = tokenInformation.UserEmail;
+
+        Result<bool> result = await userService.UpdateUserInformation(userUpdate);
+
+        if (result.IsFailed)
+        {
+            return BadRequest(new ResultFailDto(false, result.Errors));
+        }
+
+        return Ok(new ResultSuccessDto<bool>(result.IsSuccess, result.Value));
+    }
+
+    [HttpPut("password")]
+    public async Task<IActionResult> Information([FromBody] PasswordUpdateDto passwordUpdate)
+    {
+        Result<bool> result = await userService.UpdateUserPassword(passwordUpdate);
+
+        if (result.IsFailed)
+        {
+            return BadRequest(new ResultFailDto(false, result.Errors));
+        }
+
+        return Ok(new ResultSuccessDto<bool>(result.IsSuccess, result.Value));
     }
 }
